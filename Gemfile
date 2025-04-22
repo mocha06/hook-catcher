68a1f818b4d96996086a7f54a737f3b187d63d8f
Roberto Chavarria <robertomorais107@gmail.com>
2025-04-22 03:00:09 -0300

    add annotation gem

--- Gemfile
+++ Gemfile
@@ -44,6 +44,7 @@ end
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem "web-console"
+  gem 'annotate', '~> 3.2.0'
 end
 
 group :test do
--- test/models/webhook_bin_test.rb
+++ test/models/webhook_bin_test.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: webhook_bins
+#
+#  id         :integer          not null, primary key
+#  token      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require "test_helper"
 
 class WebhookBinTest < ActiveSupport::TestCase
