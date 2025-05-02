57eb9c4a2e1cbb6aafdeaddb002c39384506e61d
Roberto Chavarria <robertomorais107@gmail.com>
2025-05-02 19:47:14 -0300

    adding page for webhook requests + creating a new bin when not found

--- app/controllers/webhook_bins_controller.rb
+++ app/controllers/webhook_bins_controller.rb
@@ -2,9 +2,16 @@ class WebhookBinsController < ApplicationController
   def new
     url = SecureRandom.hex(16)
     @webhook_bin = WebhookBin.new(token: url)
+    @webhook_bin.save!
   end
 
   def create
     @webhook_bin = WebhookBin.create!(webhook_bin_params)
+  end
+
+  def show
+    @webhook_bin = WebhookBin.find_by(token: params[:token])
+
+    redirect_to new_webhook_bin_path unless @webhook_bin
   end
 end
